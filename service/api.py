715b4aa2d6645400aa454e97948016e239ff6134
lightsource <142080508+l1ghtsource@users.noreply.github.com>
2024-05-18 21:00:39 +0300

    upd api

--- service/api.py
+++ service/api.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI, File, UploadFile
-from full_pipeline import full_pipeline
+from fastapi.responses import FileResponse
+from full_pipeline import full_pipeline, table_pipe
 import shutil
 import os
+import pandas as pd
+from typing import List
+import io
 
 app = FastAPI()
 
@@ -18,6 +22,29 @@ async def run_full_pipeline(file: UploadFile = File(...)):
         os.remove(file.filename)
 
 
+if not os.path.exists("uploads"):
+    os.makedirs("uploads")
+if not os.path.exists("outputs"):
+    os.makedirs("outputs")
+
+        
+@app.post("/predict_table/")
+async def upload_file(file: UploadFile = File(...)):
+    try:
+        file_path = os.path.join("uploads", file.filename)
+        with open(file_path, "wb") as buffer:
+            buffer.write(file.file.read())
+        
+        df = table_pipe(file_path)
+        
+        output_file_path = os.path.join("outputs", "output.xlsx")
+        df.to_excel(output_file_path, index=False)
+        
+        return FileResponse(output_file_path, filename="output.xlsx")
+    except Exception as e:
+        return {"error": str(e)}
+        
+        
 @app.get("/docs")
 async def get_docs():
     return {"message": "SwaggerUI можно найти по адресу /docs"}
@@ -25,4 +52,4 @@ async def get_docs():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="90.156.216.132", port=8000)
